3a355998128c5058cb4b4b974087ff35376e2a44
champix56 <desorbaix@free.fr>
2023-09-12 16:37:40 +0100

    gestion de fetch par cmp pour etat local

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import FlexW1G from "./components/layout/FlexW1G/FlexW1G";
 import FlexH3G from "./components/layout/FlexH3G/FlexH3G";
 import MemeForm from "./components/MemeForm/MemeForm";
@@ -17,6 +17,12 @@ const App = () => {
       h: 348,
     },
   ]);
+  useEffect(() => {
+    fetch("http://localhost:7956/images")
+      .then((resp) => resp.json())
+      .then((a) => setimages(a));
+  }, []);
+
   return (
     <div className="App">
       <FlexH3G>
@@ -29,6 +35,7 @@ const App = () => {
             basePath=""
           ></MemeViewer>
           <MemeForm
+            images={images}
             meme={current}
             onMemeChange={(newMeme) => {
               setcurrent(newMeme);
