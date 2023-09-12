c2441daf86aa563e9a5ef66032264fb4747785c5
champix56 <desorbaix@free.fr>
2023-09-12 15:01:43 +0100

    ajout de type pour submit/reset

--- src/App/components/Button/Button.jsx
+++ src/App/components/Button/Button.jsx
@@ -15,13 +15,14 @@ const Button = (props) => {
     <button
       onClick={(evt) => {
         setIsClicked(true);
-        props.onClick();
+        if(undefined!==props.onClick)props.onClick();
       }}
       className={isClicked ? style.Button + " " + style.clicked : style.Button}
       style={{
         backgroundColor: props.bgColor,
         ...props.style,
       }}
+      type={props.type}
     >
       {props.children}
     </button>
@@ -31,14 +32,16 @@ const Button = (props) => {
 Button.propTypes = {
   children: PropTypes.any.isRequired,
   bgColor: PropTypes.oneOf(["skyblue", "tomato", "transparent"]),
-  onClick: PropTypes.func.isRequired,
+  onClick: PropTypes.func,
   style: PropTypes.shape({
     width: PropTypes.string,
     padding: PropTypes.string,
   }),
+  type: PropTypes.oneOf(["button", "submit", "reset", undefined]),
 };
 Button.defaultProps = {
   bgColor: "skyblue",
+  type: "button",
 };
 
 export default Button;
