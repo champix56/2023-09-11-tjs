4271a4157a3d0ccba7716a4bf56f3dee5e91aa5b
champix56 <desorbaix@free.fr>
2023-09-13 11:10:13 +0100

    gestion des changements de current

--- src/App/store/current.js
+++ src/App/store/current.js
@@ -3,27 +3,20 @@ import { DummyMeme } from '../interfaces/common';
 
 const initialState = DummyMeme;
 
-// const current = createSlice({
-//   name: 'current',
-//   initialState,
-//   reducers: {
-//     changeCurrent(state,action){
-
-//     },
-//     clearCurrent(state,action){
-
-//     }
-//   }
-// });
-
-// export const {clearCurrent,changeCurrent} = current.actions
-
-// export default current.reducer
-const currentReducer =(state=initialState,action)=>{
-    switch(action.type){
-        case 'current/change':return {...state,...action.payload};
-        case 'current/clear':return {...DummyMeme};
-        default : return state;
+const current = createSlice({
+  name: 'current',
+  initialState,
+  reducers: {
+    change(state,action){
+        Object.assign(state,action.payload);
+    },
+    clear(state){
+        delete state.id;
+        Object.assign(state,DummyMeme);
     }
-}
-export default currentReducer
+  }
+});
+
+export const {clearCurrent,changeCurrent} = current.actions
+
+export default current.reducer
